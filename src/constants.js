4e37250e9a9c277434470dc6a371a44f6c2cce81
victor <vahev92@gmail.com>
2019-10-06 12:10:38 -0500

    Smart Machine MVP

---
+++ src/constants.js
@@ -0,0 +1,8 @@
+export const WAREHOUSES = [
+    { label: 'Monterrey', value: 'MTY' },
+    { label: 'New York', value: 'NYC' },
+    { label: 'Mexico City', value: 'MEX' },
+    { label: 'Houston', value: 'HOU' },
+    { label: 'Los Angeles', value: 'LAX' },
+];
+export const CLASSIFICATION_HOST = "http://ec2-52-90-216-246.compute-1.amazonaws.com:8080"
--- src/index.js
+++ src/index.js
@@ -1,12 +1,64 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as constants from './constants';
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class SmartMachine extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            isRequestComplete: true,
+            img: "/placeholder-img.jpg"
+        }
+    }
+    onSubmit(e) {
+        console.log("sending request...")
+        e.preventDefault()
+        let formData = new FormData();
+        let location = document.getElementById("location")
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+        formData.append('attachedImage', document.getElementById('snapshot').files[0])
+        formData.append('weight', document.getElementById('weight').value)
+        formData.append('warehouseLocation', location.options[location.selectedIndex].value)
+        
+        let content = {
+            method: 'POST',
+            body: formData
+        }
+
+        fetch(`${constants.CLASSIFICATION_HOST}/api/classify`, content)
+        .then(response => { response.text().then(text => alert(text)) })
+        .catch(error => { console.log(error)})
+
+    }
+
+    render() {
+        const warehouses = []
+        for (const [index, value] of constants.WAREHOUSES.entries()) {
+            warehouses.push(<option key={index} value={value.value} >{value.label}</option>)
+        }
+        return (
+            <div className="container">
+                <h1>Warehouse Input Simulator</h1>
+                <div className="form-container">
+                    <form onSubmit={this.onSubmit} encType="multipart/form-data">
+                        <div className="fileContainer">
+                            <input name="snapshot" id="snapshot" type="file" required/>
+                            <img src={this.state.img} alt="package"/>
+                        </div>
+                        <select id="location">
+                            {warehouses}
+                        </select>
+                        <input name="weight" id="weight" type="number" required/>
+                        <input id="submit" type="submit" name="submit" value="Send"/>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <SmartMachine />,
+    document.getElementById('root')
+);
